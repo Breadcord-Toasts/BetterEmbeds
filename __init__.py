ab2f754a1c1729df3472d09d5d625dc5516e6013
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-08-09 17:43:28 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,62 @@
+import inspect
+import json
+import re
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+import breadcord
+from .constants import GITHUB_LINE_NUMBER_URL_REGEX
+
+
+class BreadAssassin(breadcord.module.ModuleCog):
+    def __init__(self, module_id: str):
+        super().__init__(module_id)
+        self.session: aiohttp.ClientSession | None = None
+
+    async def cog_load(self) -> None:
+        self.session = aiohttp.ClientSession()
+
+    async def cog_unload(self):
+        if self.session is not None:
+            await self.session.close()
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        def is_enabled(setting_key: str) -> bool:
+            return self.settings.get(setting_key).value
+
+        if is_enabled("github"):
+            await self.handle_github_url(message)
+
+    async def handle_github_url(self, message: discord.Message):
+        if not message.content:
+            return
+        for match in GITHUB_LINE_NUMBER_URL_REGEX.finditer(message.content):
+            owner, repo, branch, file_path, file_ext, l1, l2 = match.groupdict().values()
+            l1 = int(l1) - 1
+            l2 = int(l2) - 1 if l2 else l1
+
+            if not all((owner, repo, branch, file_path, l1)):
+                continue
+
+            async with self.session.get(
+                f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}.{file_ext}?ref={branch}",
+                headers={"Accept": "application/vnd.github.raw"},
+            ) as response:
+                self.logger.debug(f"Fetching {response.url}")
+                if response.status != 200:
+                    continue
+
+                lines = (await response.text()).splitlines()
+            code = "\n".join(lines[l1 : l2 + 1])
+            codeblock = f"```{file_ext or ''}\n{code}\n```"
+            if code and len(codeblock) <= 2000:
+                await message.reply(
+                    codeblock,
+                    mention_author=False,
+                )
+
+async def setup(bot: breadcord.Bot):
+    await bot.add_cog(BreadAssassin("better_embeds"))
