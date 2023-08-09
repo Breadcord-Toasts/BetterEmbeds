7d139f6c4845f43c4452eeedf20b85d4a9583b27
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-08-09 18:13:55 +0200

    Add support for using a github token (untested)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-import inspect
-import json
-import re
-
 import aiohttp
 import discord
 from discord.ext import commands
@@ -31,32 +27,36 @@ class BreadAssassin(breadcord.module.ModuleCog):
             await self.handle_github_url(message)
 
     async def handle_github_url(self, message: discord.Message):
-        if not message.content:
+        if not message.content or message.author.bot:
             return
         for match in GITHUB_LINE_NUMBER_URL_REGEX.finditer(message.content):
             owner, repo, branch, file_path, file_ext, l1, l2 = match.groupdict().values()
-            l1 = int(l1) - 1
-            l2 = int(l2) - 1 if l2 else l1
-
             if not all((owner, repo, branch, file_path, l1)):
                 continue
 
+            headers = {"Accept": "application/vnd.github.raw"}
+            if github_token := self.settings.github_token.value:
+                headers["Authorization"] = f"Bearer {github_token}"
             async with self.session.get(
                 f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}.{file_ext}?ref={branch}",
-                headers={"Accept": "application/vnd.github.raw"},
+                headers=headers
             ) as response:
                 self.logger.debug(f"Fetching {response.url}")
                 if response.status != 200:
                     continue
-
                 lines = (await response.text()).splitlines()
+
+            l1 = int(l1) - 1
+            l2 = int(l2) - 1 if l2 else l1
             code = "\n".join(lines[l1 : l2 + 1])
             codeblock = f"```{file_ext or ''}\n{code}\n```"
+
             if code and len(codeblock) <= 2000:
                 await message.reply(
                     codeblock,
                     mention_author=False,
                 )
+
 
 async def setup(bot: breadcord.Bot):
     await bot.add_cog(BreadAssassin("better_embeds"))
