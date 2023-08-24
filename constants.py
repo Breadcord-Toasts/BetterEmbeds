306d77c1c616f764b1d4a3ec3ff6697e7427b1f6
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-08-24 19:14:27 +0200

    Update regex

--- constants.py
+++ constants.py
@@ -4,13 +4,13 @@ GITHUB_LINE_NUMBER_URL_REGEX = re.compile(
     r"""
     (?<!\S)                             # Discard any match proceded by a non-space character
     https?://github\.com/               
-    (?P<owner>\w+)/                     # Repo owner
-    (?P<repo>\w+)/                      # Repo name
-    blob/(?P<branch>\w+)/               # Branch name or hash
+    (?P<owner>[\w\-.]+)/                # Repo owner
+    (?P<repo>[\w\-.]+)/                 # Repo name
+    blob/(?P<branch>[\w\-.]+)/          # Branch name or hash
     (?P<file_path>.+?)                  # File path
-                                        # Please PR some propper matching for this, 'cause I sure won't do it
     (?:\.(?P<file_ext>\w+))?            # Optional file extension
-    \#L(?P<l1>\d+)                      # Line number. # is escaped due to it being seen as a comment otherwise
+    (?:\?.+)?                           # Optional query string
+    \#L(?P<l1>\d+)                      # Line number
     (?:-L(?P<l2>\d+))?                  # Optional ending line number
     (?!\S)                              # Discard any match followed by a non-space character
     """,
