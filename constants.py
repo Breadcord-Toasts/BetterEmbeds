c07bf302a9cfef0d48d80634f5e09226d9a99e75
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-11-07 19:19:02 +0100

    Allow github links with embedding suppressed

--- constants.py
+++ constants.py
@@ -2,7 +2,11 @@ import re
 
 GITHUB_LINE_NUMBER_URL_REGEX = re.compile(
     r"""
-    (?<!\S)                             # Discard any match proceded by a non-space character
+    (?:
+        (?<!\S)                         # Discard any match proceded by a non-space character
+        |                               # Or
+        (?<=<)                          # Discard any match not proceded by a "<" character
+    )
     https?://github\.com/               
     (?P<owner>[\w\-.]+)/                # Repo owner
     (?P<repo>[\w\-.]+)/                 # Repo name
@@ -12,7 +16,11 @@ GITHUB_LINE_NUMBER_URL_REGEX = re.compile(
     (?:\?.+)?                           # Optional query string
     \#L(?P<l1>\d+)                      # Line number
     (?:-L(?P<l2>\d+))?                  # Optional ending line number
-    (?!\S)                              # Discard any match followed by a non-space character
+    (?:
+        (?!\S)                          # Discard any match followed by a non-space character
+        |                               # Or
+        (?=>)                           # Discard any match not followed by a ">" character
+    )
     """,
     flags=re.VERBOSE
 )
